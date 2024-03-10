ab86544f39ac01112826a1130d1b8bdac205028a
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-10 09:51:00 +0100

    typescript skills

---
+++ interfaces.ts
@@ -0,0 +1,6 @@
+export interface ISkill {
+	idCode: string;
+	name: string;
+	url: string;
+	description: string
+}
--- pages/skills/index.vue
+++ pages/skills/index.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
 import _skills from "~/data/skills.json";
-const skills = ref(_skills);
+import type { ISkill } from "~/interfaces";
+
+const skills = ref<ISkill[]>(_skills);
+
+const foundSkills = computed((): ISkill[] => {
+	const _foundSkills = skills.value.filter((m) =>
+		m.description.includes("React")
+	);
+	if (_foundSkills) {
+		return _foundSkills;
+	} else {
+		return [] as ISkill[];
+	}
+});
+
 </script>
 
 <template>
-	<p>There are {{ skills.length }} skills.</p>
+	<div class="mb-5">
+		<h2 class="text-xl mb-3">There are {{ skills.length }} skills.</h2>
+		<div>
+			<span v-for="skill in skills">({{ skill.name }}) </span>
+		</div>
+	</div>
+	<div class="mb-5">
+		<h2 class="text-xl mb-3">There are {{ foundSkills.length }} skills.</h2>
+		<div>
+			<span v-for="foundSkill in foundSkills"
+				>({{ foundSkill.name }})
+			</span>
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
