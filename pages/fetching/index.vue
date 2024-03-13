8280caecabff2381673aa5485c8b5004aa95027f
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-13 20:23:36 +0100

    fetch page content

--- pages/fetching/index.vue
+++ pages/fetching/index.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+const { data: products } = await useFetch("/api/products");
+// console.log("number of products", products.length); // undefined
+console.log("products", products);
+</script>
 
 <template>
-	<p>fetching page</p>
+	<p v-if="products">There are {{ products.length }} products.</p>
 </template>
 
 <style scoped></style>
