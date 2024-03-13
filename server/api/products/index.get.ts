615110e7e57b930da3eda37de35c39282a151c90
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-13 15:52:17 +0100

    data in products api

--- server/api/products/index.get.ts
+++ server/api/products/index.get.ts
@@ -1,5 +1,33 @@
 export default defineEventHandler((event) => {
-	const query = getQuery(event);
-	console.log("query", query);
-	return "you want to get all products";
+	// const query = getQuery(event);
+	// console.log("query", query);
+	return [
+		{
+			id: 1,
+			title: "Smartphone",
+			description:
+				"A high-performance smartphone with advanced features.",
+			price: 599.99,
+		},
+		{
+			id: 2,
+			title: "Laptop",
+			description: "A powerful laptop for work and entertainment.",
+			price: 1099.99,
+		},
+		{
+			id: 3,
+			title: "Headphones",
+			description:
+				"Premium noise-canceling headphones for immersive audio experience.",
+			price: 249.99,
+		},
+		{
+			id: 4,
+			title: "Smartwatch",
+			description:
+				"An elegant smartwatch with health and fitness tracking features.",
+			price: 199.99,
+		},
+	];
 });
