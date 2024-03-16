b055ca207d9ce044e233f52e1584f62b44d81bb7
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-16 19:56:59 +0100

    loading lowdb jobs

--- server/dataSources/lowdb.ts
+++ server/dataSources/lowdb.ts
@@ -1,15 +1,14 @@
-import { join } from 'path';
-import { JSONFile } from 'lowdb/node';
-import { Low } from 'lowdb';
-import type { Database } from '../../types';
+import { join } from "path";
+import { JSONFile } from "lowdb/node";
+import { Low } from "lowdb";
+import type { Database } from "../../types";
 
 const projectBasePath = process.cwd();
-const dbPathAndFileName = join(projectBasePath, 'data/db.json');
-console.log(dbPathAndFileName);
+const dbPathAndFileName = join(projectBasePath, "data/db.json");
 const adapter = new JSONFile<Database>(dbPathAndFileName);
 const db: Low<Database> = new Low<Database>(adapter, {} as Database);
-await db.read();
 
-export const getAllJobs = () => {
-	return ['111']// db.data.jobs;
-}
+export const getAllJobs = async () => {
+	await db.read();
+	return db.data.jobs;
+};
