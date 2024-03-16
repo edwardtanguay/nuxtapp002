600f96cc8cf39f3eefefd4a0936c64c4ff570f68
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-17 00:07:22 +0100

    post job works

--- server/dataSources/lowdb.ts
+++ server/dataSources/lowdb.ts
@@ -2,15 +2,25 @@ import { join } from "path";
 import { JSONFile } from "lowdb/node";
 import { Low } from "lowdb";
 import type { Database, Job, NewJob } from "../../types";
+import * as lowdb from './lowdb';
 
 const projectBasePath = process.cwd();
 const dbPathAndFileName = join(projectBasePath, "data/db.json");
 const adapter = new JSONFile<Database>(dbPathAndFileName);
 const db: Low<Database> = new Low<Database>(adapter, {} as Database);
 
+export const getNextId = async () => {
+	const jobs = await lowdb.getAllJobs();
+	const highestId = jobs.reduce((maxId, job) => {
+		return job.id > maxId ? job.id : maxId;
+	}, 0);
+	return highestId + 1;
+}
+
 export const getAllJobs = async () => {
 	await db.read();
-	return db.data.jobs;
+	const jobs: Job[] = db.data.jobs;;
+	return jobs;
 };
 
 export const deleteJob = async (id:number) => {
@@ -32,7 +42,7 @@ export const createJob = async (newJob: NewJob) => {
 	await db.read();
 	const job: Job = {
 		...newJob,
-		id: 999
+		id: await lowdb.getNextId()
 	}
 	const jobs = db.data.jobs;
 	jobs.push(job);
