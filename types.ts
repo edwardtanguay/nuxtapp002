8d525810815bc8c8dbfd7f38393135937ac68cee
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-18 09:47:16 +0100

    extended Zod

--- types.ts
+++ types.ts
@@ -29,36 +29,27 @@ export type Area = "firstArea" | "secondArea";
 export const NewJobSchema = z.object({
 	title: z.string(),
 	company: z.string(),
-	url: z.string(),
+	url: z.string().regex(/^https?:\/\/(?:www\.)?[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(?:\/[^\s]*)?$/),
 	description: z.string(),
 	skillList: z.string(),
 	publicationDate: z.string(),
 });
 export type NewJob = z.infer<typeof NewJobSchema>;
 
-export const JobSchema = z.object({
-	id: z.number(),
-	title: z.string(),
-	company: z.string(),
-	url: z.string().regex(/^https?:\/\/(?:www\.)?[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(?:\/[^\s]*)?$/),
-	description: z.string(),
-	skillList: z.string(),
-	publicationDate: z.string(),
-});
+export const JobSchema = NewJobSchema.and(
+	z.object({
+		id: z.number(),
+	})
+);
 export type Job = z.infer<typeof JobSchema>;
 
-export const FrontendJobSchema = z.object({
-	id: z.number(),
-	title: z.string(),
-	company: z.string(),
-	url: z.string().url(),
-	description: z.string(),
-	skillList: z.string(),
-	publicationDate: z.string(),
-	isOpen: z.boolean(),
-	userIsDeleting: z.boolean(),
-	isProcessing: z.boolean(),
-});
+export const FrontendJobSchema = JobSchema.and(
+	z.object({
+		isOpen: z.boolean(),
+		userIsDeleting: z.boolean(),
+		isProcessing: z.boolean(),
+	})
+);
 export type FrontendJob = z.infer<typeof FrontendJobSchema>;
 
 export type Database = {
