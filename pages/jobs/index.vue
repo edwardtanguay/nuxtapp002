4a56d77525fcb8e35c4f33b2b4f8e5abee885d7e
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-20 03:40:12 +0100

    jobs top/bottom

--- pages/jobs/index.vue
+++ pages/jobs/index.vue
@@ -8,12 +8,18 @@ import { appStore } from "~/stores/AppStore";
 		<h2 class="mb-2 text-2xl text-blue-950">
 			{{ appStore.frontendJobs.length }} Jobs
 		</h2>
-		<ul>
-			<li v-for="frontendJob in appStore.frontendJobs">
-				<span class="font-semibold">{{ frontendJob.title }}</span> -
-				{{ frontendJob.company }} ({{ frontendJob.url }})
-			</li>
-		</ul>
+		<div>
+			<div v-for="frontendJob in appStore.frontendJobs" class="mb-3 w-[40rem]">
+			<div class="bg-slate-400 p-2 rounded-t-lg">
+				<div class="font-semibold">{{ frontendJob.title }}</div>
+			</div>
+			<div class="bg-slate-300 p-2 rounded-b-lg">
+			<div>
+				{{ frontendJob.company }}
+			</div>	
+			</div>
+			</div>
+		</div>
 	</div>
 </template>
 
