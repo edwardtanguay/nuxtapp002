962b767a862e92a538142f89e4cf1789d52a99ce
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-20 03:52:08 +0100

    company skills

--- pages/jobs/index.vue
+++ pages/jobs/index.vue
@@ -9,15 +9,18 @@ import { appStore } from "~/stores/AppStore";
 			{{ appStore.frontendJobs.length }} Jobs
 		</h2>
 		<div>
-			<div v-for="frontendJob in appStore.frontendJobs" class="mb-3 w-[40rem]">
-			<div class="bg-slate-400 p-2 rounded-t-lg">
-				<div class="font-semibold">{{ frontendJob.title }}</div>
-			</div>
-			<div class="bg-slate-300 p-2 rounded-b-lg">
-			<div>
-				{{ frontendJob.company }}
-			</div>	
-			</div>
+			<div
+				v-for="frontendJob in appStore.frontendJobs"
+				class="mb-3 w-[40rem]"
+			>
+				<div class="bg-slate-400 py-2 px-3 rounded-t-lg flex justify-between">
+					<div class="font-semibold">{{ frontendJob.title }}</div>
+					<div class="text-slate-700">{{ frontendJob.publicationDate }}</div>
+				</div>
+				<div class="bg-slate-300 pb-2 pt-1 px-3 rounded-b-lg">
+					<div class="font-semibold italic">{{ frontendJob.company }}</div>
+					<div>{{ frontendJob.skillList }}</div>
+				</div>
 			</div>
 		</div>
 	</div>
