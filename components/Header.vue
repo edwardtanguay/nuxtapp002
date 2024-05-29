8d86f4697a66eec6ff6b3751c6dc01625db39c47
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-29 18:59:45 +0200

    added subittle

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,4 @@
 <template>
-	<h1 class="text-2xl py-3 px-6">Nuxt Learning Showcase</h1>
+	<h1 class="text-2xl pt-3 px-6">Nuxt Learning Showcase</h1>
+	<h2 class="italic mb-3 px-6">this site was made with <span class="text-gray-800">Nuxt 3</span>, see <a class="text-gray-800 underline" href="nnn">the code</a></h2>
 </template>
